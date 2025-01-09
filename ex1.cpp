407c66f3f140b38b70d516b88df68c545fabae38
David Maged <davidmaged1123@gmail.com>
2025-01-10 00:58:05 +0200

    costants

--- ex1.cpp
+++ ex1.cpp
@@ -5,8 +5,39 @@ using namespace std;
 
 int main()
 {
+    //integers
     int x;
-    cout << "hello "<< endl;
+    cout << "x = "<<x<<'\n';
+    //double
+    double gpa = 3.335;
+
+    cout << "gpa = "<<gpa<<'\n';
+    printf("gpa = %0.0f\n", gpa);
+
+    //character
+    char grade = 'A';
+    //char grade2 = 'ac;
+    //cout << "grade2 = "<<grade2<<'\n';
+
+
+    cout << "grade = "<<grade<<'\n';
+    //boolian
+    bool isMale = true;
+    cout << "isMale = "<<isMale<<'\n';
+    bool a = 0,b=1;
+    bool ANDgate = a && b;
+
+    cout << "ANDgate = "<<ANDgate<<'\n';
+
+    //string
+    string name = "David";
+    cout << "name = "<<name<<'\n';
+    //printf("name= %s\n", name);
+
+    
+
+
+
 
     return 0;
 }
---
+++ ex2.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const double pi = 3.14159;
+    double radius =11;
+    double circumference = 2 * pi * radius ;
+    cout<<circumference <<"cm\n";
+
+
+
+    return 0;
+
+}
+    
