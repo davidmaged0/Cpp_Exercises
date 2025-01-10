37487406b2c59c1eb5e16ee2884451b55f754fac
David Maged <davidmaged1123@gmail.com>
2025-01-10 15:29:02 +0200

    Add typedefs for int and float, initialize and print variable

---
+++ ex4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+typedef int number_t;
+typedef float fnumber_t;
+
+int main() {
+        // Your code here
+    number_t number =3;
+    fnumber_t number2 = 4.5;
+    cout<< number <<'\n';
+    cout<< number2 <<'\n';
+    printf("float number is %0.3f\n", number2); 
+    
+
+    
+
+    return 0;
+}
