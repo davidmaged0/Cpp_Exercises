60b84a3f8696db07929e64f123e35daccb30db22
David Maged <davidmaged1123@gmail.com>
2025-01-10 15:36:59 +0200

    Add using instead of typedef for int and float, initialize and print variable

--- ex4.cpp
+++ ex4.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 using namespace std;
 
-typedef int number_t;
+/*typedef int number_t;
 typedef float fnumber_t;
-
+*/
+using string = std::string;
+using number = int;
+using fnumber = float;
 int main() {
         // Your code here
+    string name = "Hello";
+    number x = 4;
+    fnumber y = 4.54;
+    cout<< name <<'\n';
+    cout<< x <<'\n';
+    printf("float number = %0.4f\n", y)    
+    /*
     number_t number =3;
     fnumber_t number2 = 4.5;
     cout<< number <<'\n';
     cout<< number2 <<'\n';
-    printf("float number is %0.3f\n", number2); 
-    
-
-    
+    printf("float number is %0.3f\n", number2); */
 
     return 0;
 }
