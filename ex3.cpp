dd49ab3a3c80d0b803b1393f40e39e89a68a398e
David Maged <davidmaged1123@gmail.com>
2025-01-10 13:31:12 +0200

    namespace_3

--- ex3.cpp
+++ ex3.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 
-namespace firstValue{
-    int x =4;
+namespace firstValue {
+    int x = 4;
 }
-namespace secondValue
-{
+
+namespace secondValue {
     int x = 5;
 }
-#include <iostream>
 
 int main() {
     //int x = 2;
-    using namespace secondValue;
-
-    std::cout << x << '\n';
-    //std::cout << firstValue::x << '\n'; 
-    //std::cout << secondValue::x << '\n';
+    // using namespace secondValue;
+    // using namespace firstValue;
+    std::cout << firstValue::x << '\n';
+    std::cout << firstValue::x << '\n'; 
+    std::cout << secondValue::x << '\n';
 
     return 0;
 }
