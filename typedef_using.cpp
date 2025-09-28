87eee8aa9ba597e2f872c9bb2228e903d5054c6b
david <davidmaged1123@gmail.com>
2025-09-28 21:40:12 +0300

    ...

--- typedef_using.cpp
+++ typedef_using.cpp
@@ -5,12 +5,19 @@
 using name_t = std::string;
 using age_t = int;
 
+void print_info(const name_t& name, age_t age)
+{
+    std::cout << name << " is " << age << " years old." << '\n';
+}   
+
 int main()
 {
     name_t name = "David";
     age_t age = 25;
 
-    std::cout << name << " is " << age << " years old." << '\n';
+    print_info(name, age);
+
+    //std::cout << name << " is " << age << " years old." << '\n';
 
 
 
