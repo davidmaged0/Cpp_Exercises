d8c2c18d163d2f1117eec78b0b0b16bc3f8cf608
david <davidmaged1123@gmail.com>
2025-10-03 16:36:40 +0300

    Add example demonstrating sizeof operator with arrays

--- ex1.cpp
+++ ex1.cpp
@@ -5,19 +5,21 @@ using namespace std;
 
 int main()
 {
-    int nums[5]={2 , 4 , 6 , 8 , 10};
-    for (int i=4; i>=0; i--)
+    int myNumbers[5] = {10, 20, 30, 40, 50};
+    cout <<"size of the entire array: "<< sizeof(myNumbers)<<"\n"; // Outputs 20 = 4 byte(int) * 5 elements
+    cout << "size of an element in myNumbers Array: "<<sizeof(myNumbers[0])<<"\n";  
+    string cars[] ={"Volvo", "BMW", "Ford"};
+    /*for (int i = 0; sizeof(cars)/sizeof(cars[0]); i++)
     {
-        cout << nums[i] << " ";
-    }
-
-    cout << "\n";
-    for (int i : nums)
-    {
-        cout << i << "\n";
-
-    }
+        cout << cars[i] << "\n";
+    }*/
+    cout << "size of an element in cars Array: "<<sizeof(cars[0]) << "\n";
+    cout << "number of elements in cars Array: "<< sizeof(cars)/sizeof(cars[0]) << "\n";
 
 
+    int length_myNumbersArray = sizeof(myNumbers)/ sizeof(myNumbers[0]);
+    cout << "number of elements in myNumbers Array: "<<length_myNumbersArray << "\n";
     return 0;
+
+
 }
