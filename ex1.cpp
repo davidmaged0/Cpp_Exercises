fd9e963103192a0c47dbbe1930daecc82ce891f9
david <davidmaged1123@gmail.com>
2025-10-03 13:55:44 +0300

    arrays practice & for loops

--- ex1.cpp
+++ ex1.cpp
@@ -5,38 +5,18 @@ using namespace std;
 
 int main()
 {
-    //integers
-    int x;
-    cout << "x = "<<x<<'\n';
-    //double
-    double gpa = 3.335;
+    int nums[5]={2 , 4 , 6 , 8 , 10};
+    for (int i=4; i>=0; i--)
+    {
+        cout << nums[i] << " ";
+    }
 
-    cout << "gpa = "<<gpa<<'\n';
-    printf("gpa = %0.0f\n", gpa);
+    cout << "\n";
+    for (int i : nums)
+    {
+        cout << i << "\n";
 
-    //character
-    char grade = 'A';
-    //char grade2 = 'ac;
-    //cout << "grade2 = "<<grade2<<'\n';
-
-
-    cout << "grade = "<<grade<<'\n';
-    //boolian
-    bool isMale = true;
-    cout << "isMale = "<<isMale<<'\n';
-    bool a = 0,b=1;
-    bool ANDgate = a && b;
-
-    cout << "ANDgate = "<<ANDgate<<'\n';
-
-    //string
-    string name = "David";
-    cout << "name = "<<name<<'\n';
-    //printf("name= %s\n", name);
-
-    
-
-
+    }
 
 
     return 0;
